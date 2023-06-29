143d7c7abb08d91299480feabef321ca506c252b
edtimer <b4timer@protonmail.com>
2023-06-29 19:35:32 +0100

    feat:add login page

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,58 +1,124 @@
 'use client'
 import {
-    Button,
-    Checkbox,
-    Flex,
-    FormControl,
-    FormLabel,
-    Heading,
-    Input,
-    Link,
-    Stack,
-    Image,
-  } from '@chakra-ui/react';
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  Image,
+} from '@chakra-ui/react';
 import WithSubnavigation from '../components/Navbar';
-  
-  export default function Authentication() {
-    return (
-        <>
-        <WithSubnavigation/>
-      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-        <Flex p={8} flex={1} align={'center'} justify={'center'}>
-          <Stack spacing={4} w={'full'} maxW={'md'}>
-            <Heading fontSize={'2xl'}>Sign in to your account</Heading>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={6}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Remember me</Checkbox>
-                <Link color={'blue.500'}>Forgot password?</Link>
-              </Stack>
-              <Button colorScheme={'blue'} variant={'solid'}>
-                Sign in
-              </Button>
+import { Formik } from 'formik';
+
+export default function Authentication() {
+  return (
+    <>
+      <WithSubnavigation />
+      <Formik
+
+        initialValues={{ email: '', password: '' }}
+
+        validate={values => {
+
+          const errors:any = {
+ 
+          };
+          
+
+          if (!values.email) {
+
+            errors.email = 'Required';
+
+          } else if (
+
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+
+          ) {
+
+            errors.email = 'Invalid email address';
+
+          }
+
+          return errors;
+
+        }}
+
+        onSubmit={(values, { setSubmitting }) => {
+
+          setTimeout(() => {
+
+            alert(JSON.stringify(values, null, 2));
+
+            setSubmitting(false);
+
+          }, 400);
+
+        }}
+
+      >
+
+        {({
+
+          values,
+
+          errors,
+
+          touched,
+
+          handleChange,
+
+          handleBlur,
+
+          handleSubmit,
+
+          isSubmitting,
+
+          /* and other goodies */
+
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+              <Flex p={8} flex={1} align={'center'} justify={'center'}>
+                <Stack spacing={4} w={'full'} maxW={'md'}>
+                  <Heading fontSize={'2xl'}>Sign in to your account</Heading>
+                  <FormControl id="email">
+                    <FormLabel>Email address</FormLabel>
+                    <Input type="email" onChange={handleChange} value={values.email} />
+                  </FormControl>
+                  <FormControl id="password">
+                    <FormLabel>Password</FormLabel>
+                    <Input type="password" onChange={handleChange} value={values.password} />
+                  </FormControl>
+                  <Stack spacing={6}>
+                    <Stack
+                      direction={{ base: 'column', sm: 'row' }}
+                      align={'start'}
+                      justify={'space-between'}>
+                      <Checkbox>Remember me</Checkbox>
+                      <Link color={'blue.500'}>Forgot password?</Link>
+                    </Stack>
+                    <Button colorScheme={'blue'} variant={'solid'} type="submit" disabled={isSubmitting}>
+                      Sign in
+                    </Button>
+                  </Stack>
+                </Stack>
+              </Flex>
+              <Flex flex={1}>
+                <Image
+                  alt={'Login Image'}
+                  objectFit={'cover'}
+                  src={
+                    'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+                  }
+                />
+              </Flex>
             </Stack>
-          </Stack>
-        </Flex>
-        <Flex flex={1}>
-          <Image
-            alt={'Login Image'}
-            objectFit={'cover'}
-            src={
-                'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
-            }
-            />
-        </Flex>
-      </Stack>
-            </>
-    );
-  }
+          </form>)}
+      </Formik>
+    </>
+  );
+}
